9f26e934d811ce51b244fdb3fab605dcac9e39fc
emir57 <emir.gurbuz06@hotmail.com>
2022-06-26 20:58:11 +0300

    get InterestsOptions values in appsettings

--- InterestApi/InterestApi/Features/Queries/Interest/CalculateInterestQueryHandler.cs
+++ InterestApi/InterestApi/Features/Queries/Interest/CalculateInterestQueryHandler.cs
@@ -1,9 +1,15 @@
-﻿using MediatR;
+﻿using InterestApi.Entities;
+using MediatR;
 
 namespace InterestApi.Features.Queries.Interest
 {
     public class CalculateInterestQueryHandler : IRequestHandler<CalculateInterestQueryRequest, CalculateInterestQueryResponse>
     {
+        private InterestsOptions _interestOptions;
+        public CalculateInterestQueryHandler(IConfiguration configuration)
+        {
+            _interestOptions = configuration.GetSection("InterestsOptions").Get<InterestsOptions>();
+        }
         public Task<CalculateInterestQueryResponse> Handle(CalculateInterestQueryRequest request, CancellationToken cancellationToken)
         {
             throw new NotImplementedException();
