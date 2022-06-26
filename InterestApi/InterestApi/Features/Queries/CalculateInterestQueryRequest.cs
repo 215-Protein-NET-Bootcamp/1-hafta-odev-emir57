82cf2c92e81354924be5e73a7c7abfc0bb02871e
emir57 <emir.gurbuz06@hotmail.com>
2022-06-26 20:22:23 +0300

    Create request and response

---
+++ InterestApi/InterestApi/Features/Queries/CalculateInterestQueryRequest.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace InterestApi.Features.Queries
+{
+    public class CalculateInterestQueryRequest : IRequest<CalculateInterestQueryResponse>
+    {
+        /// <summary>
+        /// Vade Tutarı
+        /// </summary>
+        public int MaturityAmount { get; set; }
+
+        /// <summary>
+        /// İstenen Miktar
+        /// </summary>
+        public int DesiredAmount { get; set; }
+    }
+}
---
+++ InterestApi/InterestApi/Features/Queries/CalculateInterestQueryResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace InterestApi.Features.Queries
+{
+    public class CalculateInterestQueryResponse
+    {
+        /// <summary>
+        /// Toplam ödeme tutarı
+        /// </summary>
+        public int TotalPaymentAmount { get; set; }
+
+        /// <summary>
+        /// Toplam faiz tutarı
+        /// </summary>
+        public int TotalInterestAmount { get; set; }
+    }
+}
