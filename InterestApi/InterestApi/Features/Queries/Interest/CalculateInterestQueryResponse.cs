94e866d2ddd2b4a78369dd955fcac9293656a703
emir57 <emir.gurbuz06@hotmail.com>
2022-06-26 20:50:12 +0300

    Add error and success responses

--- InterestApi/InterestApi/Features/Queries/Interest/CalculateInterestQueryResponse.cs
+++ InterestApi/InterestApi/Features/Queries/Interest/CalculateInterestQueryResponse.cs
@@ -11,5 +11,27 @@
         /// Toplam faiz tutarı
         /// </summary>
         public int TotalInterestAmount { get; set; }
+
+        public bool Succeeded { get; protected set; }
+    }
+    public class CalculateInterestSuccessQueryResponse : CalculateInterestQueryResponse
+    {
+        public CalculateInterestSuccessQueryResponse()
+        {
+            Succeeded = true;
+        }
+    }
+    public class CalculateInterestErrorQueryResponse : CalculateInterestQueryResponse
+    {
+        public string Message { get; set; }
+        public CalculateInterestErrorQueryResponse(string message) : this()
+        {
+            Message = message;
+        }
+
+        public CalculateInterestErrorQueryResponse()
+        {
+            Succeeded = false;
+        }
     }
 }
