6ff8bf81963beec6a363a7197de940a7382d669e
emir57 <emir.gurbuz06@hotmail.com>
2022-06-26 21:05:59 +0300

    Calculate Action

--- InterestApi/InterestApi/Controllers/InterestsController.cs
+++ InterestApi/InterestApi/Controllers/InterestsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using InterestApi.Features.Queries.Interest;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace InterestApi.Controllers
@@ -7,5 +9,17 @@ namespace InterestApi.Controllers
     [ApiController]
     public class InterestsController : ControllerBase
     {
+        private readonly IMediator _mediator;
+        public InterestsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Calculate(CalculateInterestQueryRequest request)
+        {
+            var response = await _mediator.Send(request);
+            return Ok(response);
+        }
     }
 }
