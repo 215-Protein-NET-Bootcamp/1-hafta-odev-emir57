3729d8277994db0e084870845ccc4d54ff5d3f78
emir57 <emir.gurbuz06@hotmail.com>
2022-06-26 21:19:38 +0300

    InterestsOptions add error properties

--- InterestApi/InterestApi/Entities/InterestsOptions.cs
+++ InterestApi/InterestApi/Entities/InterestsOptions.cs
@@ -3,5 +3,9 @@
     public class InterestsOptions
     {
         public byte InterestRate { get; set; }
+        public string DesiredAmountNullError { get; set; }
+        public string DesiredAmountNegativeError { get; set; }
+        public string MaturityAmountNullError { get; set; }
+        public string MaturityAmountNegativeError { get; set; }
     }
 }
