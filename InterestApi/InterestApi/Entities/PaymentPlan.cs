5d6d71043bdbf5dba2e4d82458c36f2a2152be6d
emir57 <emir.gurbuz06@hotmail.com>
2022-06-27 13:54:02 +0300

    PaymentPlan Configure properties

--- InterestApi/InterestApi/Entities/PaymentPlan.cs
+++ InterestApi/InterestApi/Entities/PaymentPlan.cs
@@ -2,7 +2,24 @@
 {
     public class PaymentPlan
     {
+        /// <summary>
+        /// Dönem
+        /// </summary>
+        public byte Period { get; set; }
+
+        /// <summary>
+        /// Aylık ödenen miktar
+        /// </summary>
         public int AmountPaid { get; set; }
+
+        /// <summary>
+        /// Kalan miktar
+        /// </summary>
         public int RemainingAmount { get; set; }
+
+        /// <summary>
+        /// Ödenen faiz
+        /// </summary>
+        public int InterestPaid { get; set; }
     }
 }
